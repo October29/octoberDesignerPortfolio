0d89914fd19fc07743d0a2c1d68a06902c4235df
Luis Fernando <108707353+October29@users.noreply.github.com>
2024-06-04 00:21:32 -0500

    Add files via upload

--- js/pages.js
+++ js/pages.js
@@ -1,10 +1,22 @@
 const brandDesign = document.getElementById('Diseno_de_marca');
 const disenoWeb = document.getElementById('Diseno_web_signal');
 const illustration = document.getElementById('Ilustracion');
+const whatsapp = document.getElementById('whatsapp');
+const behance = document.getElementById('behance');
+const instagram = document.getElementById('instagram');
+const twitter = document.getElementById('twitter');
+const telegram = document.getElementById('telegram');
+const mailme = document.getElementById('mailme');
 
 brandDesign.addEventListener('click', fBrandDesign);
 disenoWeb.addEventListener('click', fDisenoWeb);
 illustration.addEventListener('click', fIllustration);
+whatsapp.addEventListener('click', fWhatsapp);
+behance.addEventListener('click', fBehance);
+instagram.addEventListener('click', fInstagram);
+twitter.addEventListener('click', fTwitter);
+telegram.addEventListener('click', fTelegram);
+mailme.addEventListener('click', fMailme);
 
 function fBrandDesign() {
     window.location.href = "./page/brandDesign.html";
@@ -14,4 +26,23 @@ function fDisenoWeb() {
 }
 function fIllustration() {
     window.location.href = "./page/ilustracion.html";
+}
+
+function fWhatsapp() {
+    window.location.href = "https://wa.me/573023918771?text=Tengo%20un%20proyecto%20en%20mente!";
+}
+function fBehance() {
+    window.location.href = "https://www.behance.net/octoberdesigner";
+}
+function fInstagram() {
+    window.location.href = "https://www.instagram.com/octoberdesigner/";
+}
+function fTwitter() {
+    window.location.href = "https://twitter.com/OctoberDesigner";
+}
+function fTelegram() {
+    window.location.href = "https://t.me/OctoberDesigner";
+}
+function fMailme() {
+    window.location.href = "mailto:octoberdesigner@proton.me";
 }
